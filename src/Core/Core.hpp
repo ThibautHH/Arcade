0eeb8d22df2cf3b9cfca5f263d3567d4b271438c
LouisLanganay <louislanganay@gmail.com>
2024-03-13 11:42:02 +0100

    feat: Add Core and IModule classes
    Added the Core and IModule classes to the project. These classes will be used for the main functionality of the application.

---
+++ src/Core/Core.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** Core
+*/
+
+#ifndef CORE_HPP_
+    #define CORE_HPP_
+
+namespace Core {
+
+    class Core {
+        public:
+            Core();
+            ~Core();
+        private:
+            // LIB
+            // JEU
+    };
+}
+
+#endif /* !CORE_HPP_ */
---
+++ src/Displays/IModule.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** Arcade
+** File description:
+** IModule
+*/
+
+#ifndef IMODULE_HPP_
+    #define IMODULE_HPP_
+
+namespace Arcade::Displays {
+
+    class IModule {
+        public:
+            IModule();
+            ~IModule();
+        private:
+    };
+
+}
+
+#endif /* !IMODULE_HPP_ */
