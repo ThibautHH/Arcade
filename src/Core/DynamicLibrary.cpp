80eb14e3eaf7679f8f4c2464a0859b61afaf6882
Thibaut Hebert--Henriette <thibaut.hebert-henriette@epitech.eu>
2024-03-18 22:10:06 +0100

    feat(core) Allow reloading and creating empty DynamicLibrary

--- src/Core/DynamicLibrary.cpp
+++ src/Core/DynamicLibrary.cpp
@@ -9,8 +9,13 @@
 
 using namespace Arcade::Core;
 
+DynamicLibrary::DynamicLibrary()
+    : _handle(nullptr, dlclose)
+{
+}
+
 DynamicLibrary::DynamicLibrary(const char *path)
-    : _handle(dlexec(std::bind(dlopen, path, RTLD_LAZY)), dlclose)
+    : _handle(open(path), dlclose)
 {
 }
 
@@ -22,4 +27,19 @@ void *DynamicLibrary::dlexec(std::function<void *(void)> dlfunc)
     if (error)
         throw DynamicLibraryException(error);
     return result;
+}
+
+void *DynamicLibrary::open(const char *path)
+{
+    return dlexec(std::bind(dlopen, path, RTLD_LAZY));
+}
+
+void DynamicLibrary::loadLibrary(const char *path)
+{
+    this->_handle.reset(open(path));
+}
+
+bool DynamicLibrary::isLoaded() const noexcept
+{
+    return _handle.get() != nullptr;
 }
