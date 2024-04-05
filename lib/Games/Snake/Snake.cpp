b3ead469173a8c92bd663097dfc223e72ae8573c
LouisLanganay <louislanganay@gmail.com>
2024-04-05 11:14:24 +0200

    fix(Snake) Map creation

--- lib/Games/Snake/Snake.cpp
+++ lib/Games/Snake/Snake.cpp
@@ -10,9 +10,13 @@
 Snake::Snake()
 : _mapSize({18, 16}), _score(0), _animationTime(0)
 {
-    _map.resize(_mapSize.y);
-    for (int i = 0; i < _mapSize.y; i++)
-        _map[i].resize(_mapSize.x);
+    _map = std::vector<std::vector<Arcade::Games::ISprite *>>(_mapSize.y);
+
+    for (int i = 0; i < _mapSize.y; i++) {
+        _map[i] = std::vector<Arcade::Games::ISprite *>(_mapSize.x);
+        for (int j = 0; j < _mapSize.x; j++)
+            _map[i][j] = nullptr;
+    }
 }
 
 void Snake::init(std::string args, size_t nb_args)
@@ -32,11 +36,10 @@ void Snake::init(std::string args, size_t nb_args)
 
 void Snake::close(void)
 {
-    for (int i = 0; i < _mapSize.y; i++) {
-        for (int j = 0; j < _mapSize.x; j++) {
-            delete _map[i][j];
-        }
-    }
+    for (int i = 0; i < _mapSize.y; i++)
+        for (int j = 0; j < _mapSize.x; j++)
+            if (_map[i][j] != nullptr)
+                delete _map[i][j];
 }
 
 void Snake::generateApple(std::vector<std::vector<Arcade::Games::ISprite *>> map)
